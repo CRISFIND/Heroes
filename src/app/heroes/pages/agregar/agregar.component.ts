f33f4ec6fd315766ec440890c81f204af7f85d77
Cristhian <crisfind16@gmail.com>
2022-05-31 01:45:30 -0500

    Heroes

---
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styles: [`
+      img {
+        width :100%;
+        border-radius : 5px;
+      }
+  `]
+})
+export class AgregarComponent implements OnInit {
+
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      Id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+      superhero:'',
+      alter_ego:'',
+      characters:'',
+      first_appearance:'',
+      publisher:Publisher.DCComics,
+      alt_img:'',
+  }
+  constructor( private heroesService : HeroesService,
+               private activatedRoute : ActivatedRoute,
+               private router: Router) { }
+
+  ngOnInit(): void {
+
+    if(!this.router.url.includes('editar')){
+        return;
+    }
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id}) => this.heroesService.getHeroePorId(id))
+    )
+    .subscribe(heroe => this.heroe = heroe);
+  }
+
+  guardar(){
+    if(this.heroe.superhero.trim().length === 0){
+      return;
+    } 
+
+    if(this.heroe.id){
+        this.heroesService.actualizarheroe(this.heroe)
+                           .subscribe(heroe => console.log('Actualizando',heroe)  )
+    }else{
+      
+      this.heroesService.agregarheroe(this.heroe)
+      .subscribe(heroe => {
+       this.router.navigate(['/heroes/editar', heroe.id]);
+      })
+}
+    }
+
+    borrarHeroe(){
+      this.heroesService.borrarheroe(this.heroe.id!)
+      .subscribe(resp => {
+        this.router.navigate(['/heroes']);
+      })
+    }
+}
---
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styles: []
+})
+export class ListadoComponent implements OnInit {
+
+  constructor( private heroesService: HeroesService) { }
+
+  heroes: Heroe[] = [] ;
+
+  ngOnInit(): void {
+
+    this.heroesService.getHeroes().subscribe(heroes =>{
+        this.heroes = heroes;
+    }); //para que se dispare //console.log(resp)
+  }
+
+}
